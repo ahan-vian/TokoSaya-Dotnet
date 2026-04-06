60268a624a3a935f8464a618bc4621a31aac8d01
ahan-vian <farhanvian346@gmail.com>
2026-04-06 17:01:18 +0700

    feat: Menambahkan fitur melihat riwayat order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TokoSaya.Data;
+using TokoSaya.ViewModels;
 
 namespace TokoSaya.Controllers;
 
@@ -18,11 +20,42 @@ public class OrderController : Controller
     public IActionResult Index()
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if(userId == null || userId == "0")
+        if (userId == null || userId == "0")
         {
             return BadRequest();
         }
-        var orderHeaders =_context.OrderHeaders.Where(u=> u.ApplicationUserId == userId).ToList();
+        var orderHeaders = _context.OrderHeaders.Where(u => u.ApplicationUserId == userId).ToList();
         return View(orderHeaders);
+    }
+
+    // [HttpGet]
+    // public IActionResult Details(int id)
+    // {
+    //     OrderVM orderVM = new OrderVM()
+    //     {
+    //         OrderHeader = _context.OrderHeaders.FirstOrDefault(u => u.Id == id)!,
+    //         OrderDetail = _context.OrderDetails.Include(u => u.Product).Where(u => u.OrderHeaderId == id).ToList()
+    //     };
+    //     if (orderVM.OrderHeader == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     return View(orderVM);
+    // }
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        OrderVM orderVM = new OrderVM()
+        {
+            OrderHeader = _context.OrderHeaders.FirstOrDefault(u => u.Id == id)!,
+            OrderDetail = _context.OrderDetails.Include(u => u.Product).Where(u => u.OrderHeaderId == id).ToList()
+        };
+
+        if (orderVM.OrderHeader == null)
+        {
+            return NotFound();
+        }
+
+        return View(orderVM);
     }
 }
